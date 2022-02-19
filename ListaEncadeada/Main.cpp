c4755ca0451b2467d468df84984376cf8b106a2b
thalisson <thalissongabriel1999@gmail.com>
2022-02-18 21:53:21 -0300

    Ponteiros inteligentes

--- ListaEncadeada/Main.cpp
+++ ListaEncadeada/Main.cpp
@@ -1,25 +1,26 @@
 #include<iostream>
+#include<memory>
 #include "DynamicStack.h"
 #include "DynamicQueue.h"
 int main()
 {
-	DynamicStack Stack = DynamicStack();
-	Stack.Insert(1);
-	Stack.Insert(2);
-	Stack.Insert(3);
-	Stack.Pop();
-	Stack.Pop();
-	Stack.Show();
+	std::unique_ptr<DynamicStack> Stack(new DynamicStack());
+	Stack->Insert(1);
+	Stack->Insert(2);
+	Stack->Insert(3);
+	Stack->Pop();
+	Stack->Pop();
+	Stack->Show();
 
 	std::cout << "\n";
 
-	DynamicQueue Queue = DynamicQueue();
-	Queue.Insert(1);
-	Queue.Insert(2);
-	Queue.Insert(3);
-	Queue.Remove();
-	Queue.Remove();
-	Queue.Show();
+	std::unique_ptr<DynamicQueue> Queue(new DynamicQueue());
+	Queue->Insert(1);
+	Queue->Insert(2);
+	Queue->Insert(3);
+	Queue->Remove();
+	Queue->Remove();
+	Queue->Show();
 	system("PAUSE");
 	return 0;
 }
